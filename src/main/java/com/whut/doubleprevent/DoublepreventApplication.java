f34b60850df9ee2cf0f9f47d18762b00f0c2a3c5
luodi <2995254829@qq.com>
2019-10-08 20:43:14 +0800

    first commit

---
+++ src/main/java/com/whut/doubleprevent/DoublepreventApplication.java
@@ -0,0 +1,13 @@
+package com.whut.doubleprevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoublepreventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoublepreventApplication.class, args);
+    }
+
+}
